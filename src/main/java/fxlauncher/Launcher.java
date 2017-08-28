c6c7fb3215e58f1baf7bb133781d5344665c9192
jricardo17 <>
2017-08-28 11:20:14 -0300

    Added the possibility of custom error message. Added the possibility of customizing the error dialogs.

--- src/main/java/fxlauncher/Launcher.java
+++ src/main/java/fxlauncher/Launcher.java
@@ -2,26 +2,28 @@ package fxlauncher;
 
 import com.sun.javafx.application.ParametersImpl;
 import com.sun.javafx.application.PlatformImpl;
+import java.io.*;
+import java.net.URI;
+import java.net.URL;
+import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import static javafx.application.Application.launch;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
-import javafx.scene.control.TextArea;
+import javafx.scene.control.DialogPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import java.io.*;
-import java.net.URI;
-import java.util.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 @SuppressWarnings("unchecked")
 public class Launcher extends Application {
+
     private static final Logger log = Logger.getLogger("Launcher");
 
     private Application app;
@@ -31,8 +33,15 @@ public class Launcher extends Application {
     private StackPane root;
 
     private final AbstractLauncher superLauncher = new AbstractLauncher<Application>() {
+
+        private final String defaultMessageTitle = "Error";
+
+        private final String defaultMessageHeaderText = "Unable to connect to application server.";
+
+        private final String defaultMessageContentText = "Check your network connection and try again.";
+
         @Override
-        protected Parameters getParameters() {
+        protected Application.Parameters getParameters() {
             return Launcher.this.getParameters();
         }
 
@@ -43,8 +52,8 @@ public class Launcher extends Application {
 
         @Override
         protected void createApplication(Class<Application> appClass) {
-            PlatformImpl.runAndWait(() ->
-            {
+            PlatformImpl.runAndWait(()
+                    -> {
                 try {
                     app = appClass.newInstance();
                 } catch (Throwable t) {
@@ -57,23 +66,45 @@ public class Launcher extends Application {
         protected void reportError(String title, Throwable error) {
             log.log(Level.WARNING, title, error);
 
-            Platform.runLater(() ->
-            {
-                Alert alert = new Alert(Alert.AlertType.ERROR);
-                alert.setTitle(title);
-                alert.setHeaderText(String.format("%s\ncheck the logfile 'fxlauncher.log, usually in the %s directory", title, System.getProperty("java.io.tmpdir")));
-//            alert.setHeaderText(title+"\nCheck the logfile usually in the "+System.getProperty("java.io.tmpdir") + "directory");
-                alert.getDialogPane().setPrefWidth(600);
+            Platform.runLater(() -> {
 
-                ByteArrayOutputStream out = new ByteArrayOutputStream();
-                PrintWriter writer = new PrintWriter(out);
-                error.printStackTrace(writer);
-                writer.close();
-                TextArea text = new TextArea(out.toString());
-                alert.getDialogPane().setContent(text);
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                URL url = this.getClass().getResource("/styles/dialogs-style.css");
+
+                if (url != null) {
+                    DialogPane dialogPane = alert.getDialogPane();
+                    dialogPane.getStylesheets().add(url.toExternalForm());
+                }
+
+                InputStream messageStream = this.getClass().getResourceAsStream("/properties/error-message.properties");
+
+                if (messageStream != null) {
+
+                    Properties p = new Properties();
+
+                    try {
+
+                        p.load(messageStream);
+                        alert.setTitle(p.getProperty("message.title", this.defaultMessageTitle));
+                        alert.setHeaderText(p.getProperty("message.header.text", this.defaultMessageHeaderText));
+                        alert.setContentText(p.getProperty("message.content.text", this.defaultMessageContentText));
+
+                    } catch (IOException ex) {
+                        log.log(Level.WARNING, String.format("Error during %s phase", superLauncher.getPhase()), ex);
+                        System.exit(1);
+                    }
+
+                } else {
+
+                    alert.setTitle(this.defaultMessageTitle);
+                    alert.setHeaderText(this.defaultMessageHeaderText);
+                    alert.setContentText(this.defaultMessageContentText);
+
+                }
 
                 alert.showAndWait();
                 Platform.exit();
+
             });
         }
 
@@ -83,32 +114,36 @@ public class Launcher extends Application {
             Platform.runLater(() -> Thread.currentThread().setContextClassLoader(classLoader));
         }
 
-
     };
 
     /**
-     * Check if a new version is available and return the manifest for the new version or null if no update.
+     * Check if a new version is available and return the manifest for the new
+     * version or null if no update.
      *
-     * Note that updates will only be detected if the application was actually launched with FXLauncher.
+     * Note that updates will only be detected if the application was actually
+     * launched with FXLauncher.
      *
      * @return The manifest for the new version if available
      */
     public static FXManifest checkForUpdate() throws IOException {
         // We might be called even when FXLauncher wasn't used to start the application
-        if (AbstractLauncher.manifest == null) return null;
+        if (AbstractLauncher.manifest == null) {
+            return null;
+        }
         FXManifest manifest = FXManifest.load(URI.create(AbstractLauncher.manifest.uri + "/app.xml"));
         return manifest.equals(AbstractLauncher.manifest) ? null : manifest;
     }
 
-
     /**
-     * Initialize the UI Provider by looking for an UIProvider inside the launcher
-     * or fallback to the default UI.
+     * Initialize the UI Provider by looking for an UIProvider inside the
+     * launcher or fallback to the default UI.
      * <p>
      * A custom implementation must be embedded inside the launcher jar, and
-     * /META-INF/services/fxlauncher.UIProvider must point to the new implementation class.
+     * /META-INF/services/fxlauncher.UIProvider must point to the new
+     * implementation class.
      * <p>
-     * You must do this manually/in your build right around the "embed manifest" step.
+     * You must do this manually/in your build right around the "embed manifest"
+     * step.
      */
     public void init() throws Exception {
         Iterator<UIProvider> providers = ServiceLoader.load(UIProvider.class).iterator();
@@ -139,7 +174,7 @@ public class Launcher extends Application {
                 filesUpdated[0] = superLauncher.syncFiles();
             } catch (Exception ex) {
                 log.log(Level.WARNING, String.format("Error during %s phase", superLauncher.getPhase()), ex);
-                if(superLauncher.checkIgnoreUpdateErrorSetting()) {
+                if (superLauncher.checkIgnoreUpdateErrorSetting()) {
                     superLauncher.reportError(String.format("Error during %s phase", superLauncher.getPhase()), ex);
                     System.exit(1);
                 }
@@ -163,17 +198,19 @@ public class Launcher extends Application {
         app.init();
         superLauncher.setPhase("Application Start");
         log.info("Show whats new dialog? " + showWhatsnew);
-        PlatformImpl.runAndWait(() ->
-        {
+        PlatformImpl.runAndWait(()
+                -> {
             try {
-                if (showWhatsnew && superLauncher.getManifest().whatsNewPage != null)
+                if (showWhatsnew && superLauncher.getManifest().whatsNewPage != null) {
                     showWhatsNewDialog(superLauncher.getManifest().whatsNewPage);
+                }
 
                 // Lingering update screen will close when primary stage is shown
                 if (superLauncher.getManifest().lingeringUpdateScreen) {
                     primaryStage.showingProperty().addListener(observable -> {
-                        if (stage.isShowing())
+                        if (stage.isShowing()) {
                             stage.close();
+                        }
                     });
                 } else {
                     stage.close();
@@ -203,8 +240,8 @@ public class Launcher extends Application {
     private void createUpdateWrapper() {
         superLauncher.setPhase("Update Wrapper Creation");
 
-        Platform.runLater(() ->
-        {
+        Platform.runLater(()
+                -> {
             Parent updater = uiProvider.createUpdater(superLauncher.getManifest());
             root.getChildren().clear();
             root.getChildren().add(updater);
@@ -212,7 +249,8 @@ public class Launcher extends Application {
     }
 
     public void stop() throws Exception {
-        if (app != null)
+        if (app != null) {
             app.stop();
+        }
     }
 }
